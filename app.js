451150ac12760f6808a3da53eb5d9d3b12f89ec9
Deepak Gohil <deepakgohil@Deepaks-MacBook-Pro.local>
2023-04-14 01:16:52 +0530

    event stream added

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const express = require('express')
 const mqtt = require('mqtt')
 const mongoose = require('mongoose')
+const cors = require("cors")
 const Data = require('./models/data');
 
-
-const app = express();
-app.use(express.json());
-
-const client = mqtt.connect("mqtt://10.90.0.42", { port: 1883, username: 'admin', password: 'hivemq' })
-
-const connectDB = mongoose.connect("mongodb://10.90.0.42:27017/intellify", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("connected"))
-    .catch((err) => console.log(err));
-
-
+let connection = null
 
 // Subscribe variables
 let sub_topic = 'intellify/pub';
 let sub_options = { qos: 0 };
+
+const app = express();
+const client = mqtt.connect("mqtt://10.90.0.42", { port: 1883, username: 'admin', password: 'hivemq' })
+const connect = () => {
+    mongoose.connect("mongodb://10.90.0.42:27017/intellify")
+        .then(() => console.log("connected"))
+        .catch((err) => { throw new Error(err) })
+}
+
+
+// middlewares
+app.use(cors())
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.setHeader("Content-Type", "text/event-stream")
+    res.setHeader("Keep-Alive", "max=100")
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader("Vary", "Origin")
+    connection = res
+
+    req.on("close", () => {
+        connection = null
+    })
+})
+
+app.get("/hello", (req, res) => {
+    res.send("hello world!")
+})
 
 
 // Subscribe
@@ -32,14 +52,18 @@ client.on('connect', async function () {
         }
     })
 
-    client.on("message", function (topic, payload) {
+    client.on("message", async function (topic, payload) {
         const data = JSON.parse(payload.toString())
+        console.log(data)
         const newData = new Data({
             temperature: data.temp,
             humidity: data.hum,
             soilMoisture: data.soil
         });
-        newData.save();
+        await newData.save();
+        if (connection != null) {
+            connection.write(`data: ${payload.toString()}\n\n`)
+        }
     })
 })
 
@@ -59,14 +83,6 @@ client.on("offline", function () {
 });
 
 app.listen(3000, () => {
+    connect()
     console.log('Server is running on port 3000');
 });
-
-
-
-
-
-
-
-
-
